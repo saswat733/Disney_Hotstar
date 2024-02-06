3b9b8a149b0cb3eaefd38dfb1ad7ac27ecff09bc
saswat <saswatsingh95747@gmail.com>
2024-02-06 23:38:21 +0530

    final changes made

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
   ],
   resolve: {
     alias: {
@@ -11,6 +12,17 @@ export default defineConfig({
       'firebase/auth': 'firebase/auth',
       'firebase/firestore': 'firebase/firestore',
       'firebase/storage': 'firebase/storage',
+    },
+  },
+  build: {
+    chunkSizeWarningLimit: 1000, // Adjust the chunk size warning limit as needed
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          // Add more manual chunks as needed based on your project
+        },
+      },
     },
   },
 });
